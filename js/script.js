c49631b8320585a335b908289a948ea30df097fc
aeueoe <drom.jul@gmail.com>
2024-06-09 17:18:20 +0200

    upd. jQuery selectors

--- js/script.js
+++ js/script.js
@@ -1,51 +1,48 @@
-function newItem() {
-  //1. Adding a new task to the list of tasks:
-  let li = document.createElement("li");
-  let inputValue = document.getElementById("input").value;
-  let text = document.createTextNode(inputValue);
-  li.appendChild(text);
-
-  if (inputValue === "") {
-    alert("Write something!");
-  } else {
-    let list = document.querySelector("#list");
-    list.appendChild(li);
-  }
-
-  //2. Crossing out a task from the list of tasks:
-  function crossOut() {
-    li.classList.toggle("strike");
-  }
-
-  li.addEventListener("dblclick", crossOut);
-
-  // 3. Adding the edit button:
-  let editButton = document.createElement("button");
-  editButton.appendChild(document.createTextNode("Edit"));
-  editButton.className = "editButton";
-  li.appendChild(editButton);
-
-  editButton.addEventListener("click", editListItem);
-
-  function editListItem() {
-    let newValue = prompt("Edit your task:", inputValue);
-    if (newValue) {
-      li.childNodes[0].nodeValue = newValue;
-      inputValue = newValue;
+$(document).ready(function () {
+  function newItem() {
+    //1. Adding a new task to the list of tasks:
+    let inputValue = $("#input").val();
+    if (inputValue === "") {
+      alert("Write something!");
+      return;
     }
+
+    let li = $("<li></li>").text(inputValue);
+
+    //2. Crossing out a task from the list of tasks:
+    li.on("dblclick", function () {
+      $(this).toggleClass("strike");
+    });
+
+    // 3. Adding the edit button:
+    let editButton = $("<button>Edit</button>").addClass("editButton");
+    li.append(editButton);
+
+    editButton.on("click", function () {
+      let newValue = prompt("Edit your task:", inputValue);
+      if (newValue) {
+        li.contents().first()[0].textContent = newValue;
+        inputValue = newValue;
+      }
+    });
+
+    //4(i). Adding the delete button "X":
+    let crossOutButton = $("<button>❌</button>").addClass("deleteButton");
+    li.append(crossOutButton);
+
+    crossOutButton.on("click", function () {
+      li.addClass("delete");
+    });
+
+    // Append the new task to the list
+    $("#list").append(li);
+
+    // 6. Reordering the items:
+    $("#list").sortable();
+
+    // Reset the input field
+    $("#input").val("");
   }
 
-  //4(i). Adding the delete button "X":
-  let crossOutButton = document.createElement("crossOutButton");
-  crossOutButton.appendChild(document.createTextNode("❌"));
-  li.appendChild(crossOutButton);
-
-  crossOutButton.addEventListener("click", deleteListItem);
-  //4(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
-  function deleteListItem() {
-    li.classList.add("delete");
-  }
-
-  // 6. Reordering the items:
-  $("#list").sortable();
-}
+  $("#button").click(newItem);
+});
